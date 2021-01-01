95551a33c48128a8abc12fae52cdd66d386c7382
g-mahendra <mahendragharage@gmail.com>
2021-01-01 18:34:59 +0530

    Last commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const change = () => {
-  document.getElementById("title").style.backgroundColor = "blue";
+const validate = () => {
+  const email = document.forms["signin"]['email'];
+  const password = document.forms['signin']['password'];
+
+  if(email===""){
+    alert("Email is empty");
+  }
+  if(password===""){
+    alert("Email is empty");
+  }
 };
